81094ddf8bc63517637de5d296f1e05090d52d79
Dmitry Nazarov <mail@nazarov.tech>
2018-08-16 01:30:22 +0300

    Improve file structure

---
+++ fedsfm_data/data.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def gather():
+    url = 'http://fedsfm.ru/documents/terrorists-catalog-portal-act'
+    response = requests.get(url)
+
+    assert response.status_code == 200
+    assert response.content
+
+    raw = response.content.decode('utf-8')
+
+    as_list = raw.split('<li>')
+
+    assert 8000 < len(as_list) < 10000
+    return as_list
--- fedsfm_data/run.py
+++ fedsfm_data/run.py
@@ -1,14 +1,4 @@
-import requests
+from data import gather
 
 if __name__ == "__main__":
-    url = 'http://fedsfm.ru/documents/terrorists-catalog-portal-act'
-    response = requests.get(url)
-
-    assert response.status_code == 200
-    assert response.content
-
-    raw = response.content.decode('utf-8')
-
-    as_list = raw.split('<li>')
-
-    assert 8000 < len(as_list) < 10000
+    gather()
