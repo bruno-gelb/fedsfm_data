ccb1150e5783158bc9961cf4b87badccda8a6249
Dmitry Nazarov <mail@nazarov.tech>
2018-08-16 02:50:05 +0300

    Add more known regions to data

--- fedsfm_data/data.py
+++ fedsfm_data/data.py
@@ -3,6 +3,7 @@ import requests
 
 def place_to_region(place):
     place = place.strip()
+
     if 'ЧИАССР' in place:
         region = 'Чечено-Ингушская ССР'
     elif 'АССР' in place and 'ЧИАССР' not in place:
@@ -11,14 +12,31 @@ def place_to_region(place):
         region = 'Дагестан'
     elif 'Чечен' in place or 'чечен' in place or 'ЧЕЧЕН' in place:
         region = 'Чечня'
-    elif 'КАБАРДИН' in place:
+    elif 'ИНГУШЕТИЯ' in place:
+        region = 'Ингушетия'
+    elif 'КАБАРДИН' in place or ' КБР' in place:
         region = 'Кабардино-Балкария'
+    elif 'СТАВРОПОЛЬ' in place:
+        region = 'Ставропольский край'
     elif 'БАШКОРТОСТАН' in place:
         region = 'Башкортостан'
+    elif 'ТАДЖИК' in place:
+        region = 'Таджикистан'
     elif 'ТАТАРС' in place:
         region = 'Татарстан'
+    elif 'КЫРГЫЗ' in place or 'КИРГИЗ' in place:
+        region = 'Киргизия'
+    elif 'КАЗАХ' in place:
+        region = 'Казахстан'
+    elif 'УЗБЕК' in place:
+        region = 'Узбекистан'
+    elif 'ЧЕЛЯБИН' in place:
+        region = 'Челябинская область'
+    elif 'МОСКОВ' in place or 'МОСКВ' in place:
+        region = 'Москва'
     else:
         region = 'undefined'
+        
     return region
 
 
