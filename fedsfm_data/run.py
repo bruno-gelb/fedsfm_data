b527f896f5a5265fb9870a32c9f0090893ac24a9
Dmitry Nazarov <mail@nazarov.tech>
2018-08-16 01:09:04 +0300

    Split raw body to list of string

--- fedsfm_data/run.py
+++ fedsfm_data/run.py
@@ -3,6 +3,12 @@ import requests
 if __name__ == "__main__":
     url = 'http://fedsfm.ru/documents/terrorists-catalog-portal-act'
     response = requests.get(url)
-    
+
     assert response.status_code == 200
     assert response.content
+
+    raw = response.content.decode('utf-8')
+
+    as_list = raw.split('<li>')
+
+    assert 8000 < len(as_list) < 10000
