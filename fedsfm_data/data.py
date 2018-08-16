7beb2ccb61d3ca5ea87d7d43b6ac11a60720f944
Dmitry Nazarov <mail@nazarov.tech>
2018-08-16 04:05:02 +0300

    Move geo logic out of the data file

--- fedsfm_data/data.py
+++ fedsfm_data/data.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 import requests
 
+from geo import place_to_region
+
 
 def clean_birthday(raw_birthday):
     birthday = raw_birthday. \
@@ -11,47 +13,6 @@ def clean_birthday(raw_birthday):
         replace('г.р.', '')
 
     return birthday.strip()
-
-
-def place_to_region(place):
-    place = place.strip()
-
-    if 'ЧИАССР' in place:
-        region = 'Чечено-Ингушская ССР'
-    elif 'АССР' in place and 'ЧИАССР' not in place:
-        region = 'Азербайджанская ССР'
-    elif 'ДАГЕСТАН' in place:
-        region = 'Дагестан'
-    elif 'Чечен' in place or 'чечен' in place or 'ЧЕЧЕН' in place:
-        region = 'Чечня'
-    elif 'ИНГУШЕТИЯ' in place:
-        region = 'Ингушетия'
-    elif 'КАБАРДИН' in place or ' КБР' in place:
-        region = 'Кабардино-Балкария'
-    elif 'СТАВРОПОЛЬ' in place:
-        region = 'Ставропольский край'
-    elif 'БАШКОРТОСТАН' in place:
-        region = 'Башкортостан'
-    elif 'ТАДЖИК' in place:
-        region = 'Таджикистан'
-    elif 'ТАТАРС' in place:
-        region = 'Татарстан'
-    elif 'КЫРГЫЗ' in place or 'КИРГИЗ' in place:
-        region = 'Киргизия'
-    elif 'КАЗАХ' in place:
-        region = 'Казахстан'
-    elif 'УЗБЕК' in place:
-        region = 'Узбекистан'
-    elif 'ЧЕЛЯБИН' in place:
-        region = 'Челябинская область'
-    elif 'АЛТАЙ' in place:
-        region = 'Алтайский край'
-    elif 'МОСКОВ' in place or 'МОСКВ' in place:
-        region = 'Москва'
-    else:
-        region = 'undefined'
-
-    return region
 
 
 def strings_to_dicts(strings_list):
